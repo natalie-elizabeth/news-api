2ad4057e1ddcf911da2c79183a80e1306bffc024
Natalie <natalie.elizabeth@andela.com>
2017-05-04 16:35:08 +0300

    <Chore> <CP1> setup the environment
    Signed-off-by: Natalie <natalie.elizabeth@andela.com>

---
+++ src/app/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                Hello maginc appp
+            </div>
+        );
+    }
+}
+
+
+render(
+    <App />,
+    document.getElementById('app')
+);
+
+
