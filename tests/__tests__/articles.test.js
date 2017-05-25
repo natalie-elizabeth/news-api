5c3c76bfb0112afa3656e2a9af62a64ec30d8cd8
Natalie <natalie.elizabeth@andela.com>
2017-05-25 11:25:31 +0300

    articles test

--- tests/__tests__/articles.test.js
+++ tests/__tests__/articles.test.js
@@ -7,24 +7,4 @@ describe('Articles', () => {
     const wrapper = shallow(<div></div>);
     expect(wrapper.find('div').node).toBeDefined();
   });
-  /*it('should render articles in the divs', () => {
-    const wrapper = shallow(<Articles />);
-    expect(wrapper.containsMatchingElement(
-      <div className="articles">
-
-      </div>
-    ));
-  });*/
-  // it('asserts that articles initial state is an empty array', () => {
-  //   const wrapper = shallow(<Articles />);
-  //   expect(wrapper.state().articles).toEqual([]);
-  // });
-  it('should check if articles are being rendered', () => {
-    const wrapper = shallow(<Articles />);
-    expect(wrapper.containsMatchingElement(
-      <div >
-      </div>
-    ));
-  });
-
 });
